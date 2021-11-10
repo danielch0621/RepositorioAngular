29a04b9496e11861154b085faac1b1b483c62141
danielch0621 <danielzane0291@gmail.com>
2021-11-09 18:58:44 -0600

    commit3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-/*
+
 @NgModule({
   declarations: [
     AppComponent
@@ -15,4 +15,3 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-*/
